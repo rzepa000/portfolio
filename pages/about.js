b7ea90fb52269d7a3ebaf53a64ecfbb8d8609983
Jakub Rzepinski <rzepa8@gmail.com>
2023-03-11 19:06:58 +0000

    added content and routing

---
+++ pages/about.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ContainerBlock from "../components/ContainerBlock";
+import AboutMe from "../components/AboutMe";
+
+
+export default function about() {
+  return (
+    <ContainerBlock>
+      <AboutMe />
+    </ContainerBlock>
+  );
+}
